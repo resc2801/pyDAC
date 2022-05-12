5da3cb11c0d321f0181823a5c1f82e0e78ead6bc
rmrschub <rene.schubotz@dfki.de>
2022-05-12 15:46:43 +0200

    how did this github workflow work?

--- pydac/__init__.py
+++ pydac/__init__.py
@@ -1,1 +1,10 @@
+"""
+pyDAC.
+pyDAC (python Directly Addressable Codes) offers a variable-length encoding scheme for (unsigned) integers with random access to any element of the encoded sequence.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'René Schubotz'
+__credits__ = 'German Research Centre for Artificial Intelligence'
+
 from .DAC import *
